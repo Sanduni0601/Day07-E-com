3bf4da3790c5eb1ae367e6b86989e4a1a9072033
Sanduni0601 <sanduni06nisansala@gmail.com>
2025-05-01 17:03:01 +0530

    database connection setting

---
+++ day07/src/test/java/fas/ict/day07/Day07ApplicationTests.java
@@ -0,0 +1,13 @@
+package fas.ict.day07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
